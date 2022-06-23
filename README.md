76ca242f8e482751cde888617811225474a8aa12
EnderCraft-Dev <4soles4@gmail.com>
2022-06-23 11:37:30 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Adivina el numero (Web)
+XD
+
+
+[Adivina el número](https://adivina-el-numero-web.vercel.app/)
---
+++ static/js/app.js
@@ -0,0 +1,57 @@
+function checker(usernum, randomnum)
+{
+    if(usernum == randomnum)
+    {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+function randomNumber()
+{
+    let random = Math.floor(Math.random() * 11);
+    console.log(random)
+    return random;
+}
+let random = randomNumber();
+
+
+function getUserNumber()
+{
+    let number = document.getElementById("userNumber").value;
+    try{
+        if(number.length > 0)
+        {
+            let numberInt = parseInt(number);
+        if(typeof(numberInt) === 'number')
+        {
+            let fails = 0;
+            let life = 1;
+
+            let usernumber = numberInt;
+
+            let isCorrectAnswer = checker(usernumber, random);
+            if(isCorrectAnswer === true)
+            {
+                alert("Ganaste!!!");
+                location.reload();
+          }else {
+            fails += 1;
+            life -=1;
+            if(life == 0 && fails == 1)
+            {
+                alert("Perdiste :(");
+                location.reload();
+            }
+          }
+        }else{
+            alert("Error!");
+        }
+      }else{
+        alert("Error! Ingresa un número!")
+      }
+    }catch(e){
+        alert("Error!");
+    }
+}
